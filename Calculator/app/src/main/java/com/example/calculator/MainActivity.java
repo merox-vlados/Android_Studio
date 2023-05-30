02c08b7ce6403f0419fe4b4b131dea7a259768c4
merox-vlados <merox-vlados@mail.ru>
2023-05-30 10:11:14 +0300

    commit 30.05.2023

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editTextAnswer = findViewById(R.id.editTextAnswer);
+        Button buttonAnswer = findViewById(R.id.buttonAnswer);
+        TextView textViewIncorrectAnswer = findViewById(R.id.textViewIncorrectAnswer);
+        TextView textViewCorrectAnswer = findViewById(R.id.textViewCorrectAnswer);
+        TextView textViewExample = findViewById(R.id.textViewExample);
+
+        textViewExample.setText(R.string.newExample);
+
+        buttonAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editTextAnswer.getText().toString();
+                if(text.equals("25")) {
+                    textViewCorrectAnswer.setVisibility(View.VISIBLE);
+                    textViewIncorrectAnswer.setVisibility(View.GONE);
+                } else {
+                    textViewCorrectAnswer.setVisibility(View.GONE);
+                    textViewIncorrectAnswer.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
     }
 }
