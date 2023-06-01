505cbc1f6f5f80d497dfeafd30e83aee437c6670
merox-vlados <merox-vlados@mail.ru>
2023-06-01 17:56:11 +0300

    commit 01.06.2023

--- Messenger/app/src/main/java/com/example/messenger/MainActivity.java
+++ Messenger/app/src/main/java/com/example/messenger/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.messenger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonSendMessage = findViewById(R.id.buttonSendMessage);
+        EditText editTextMessage = findViewById(R.id.editText);
+        buttonSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = editTextMessage.getText().toString();
+                launchNextScreen(message);
+            }
+        });
+    }
+    protected void launchNextScreen(String message) {
+        Intent intent = new Intent(this, ReceivedMessageActivity.class);
+        intent.putExtra("message", message);
+        startActivity(intent);
     }
 }
--- Messenger/app/src/main/java/com/example/messenger/ReceivedMessageActivity.java
+++ Messenger/app/src/main/java/com/example/messenger/ReceivedMessageActivity.java
@@ -2,7 +2,9 @@ package com.example.messenger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReceivedMessageActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class ReceivedMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_received_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("message");
+        TextView textView = findViewById(R.id.textViewMessage);
+        textView.setText(message);
+
+
     }
 }
