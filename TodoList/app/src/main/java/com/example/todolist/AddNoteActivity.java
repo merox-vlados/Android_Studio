50941618aa331a7e32808eda080aee1059e28873
merox-vlados <merox-vlados@mail.ru>
2023-06-16 22:37:31 +0300

    commit 16.06.2023

--- TodoList/app/src/main/java/com/example/todolist/AddNoteActivity.java
+++ TodoList/app/src/main/java/com/example/todolist/AddNoteActivity.java
@@ -19,6 +19,8 @@ public class AddNoteActivity extends AppCompatActivity {
 
     private Button buttonSave;
 
+    private Database database = Database.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +52,11 @@ public class AddNoteActivity extends AppCompatActivity {
             ).show();
         }
         int priority = getPriority();
+        int id = database.getNotes().size();
+        Note note = new Note(id, text, priority);
+        database.add(note);
+
+        finish();
     }
 
     private int getPriority() {
