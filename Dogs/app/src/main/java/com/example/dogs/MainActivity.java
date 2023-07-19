eb7d99ff093f632dfd6a5c898e7d8b9d38e02bf8
merox-vlados <merox-vlados@mail.ru>
2023-07-19 07:36:00 +0300

    commit 19.07.2023

---
+++ Dogs/app/src/main/java/com/example/dogs/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.dogs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String BASE_URL = "https://dog.ceo/api/breeds/image/rando";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadDogImage();
+    }
+
+    private void loadDogImage() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(BASE_URL);
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    InputStream inputStream = urlConnection.getInputStream();
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                    String result = bufferedReader.readLine();
+                    Log.d("MainActivity", result);
+                } catch (Exception e) {
+                    Log.d("MainActivity", e.toString());
+                }
+            }
+        }).start();
+    }
+}
