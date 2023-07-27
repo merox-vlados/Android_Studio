5b4853945d73ccf91c715e926f42eb8d43418693
merox-vlados <merox-vlados@mail.ru>
2023-07-27 07:31:03 +0300

    commit 27.07.2023

--- Movies/app/src/main/java/com/example/movies/ApiService.java
+++ Movies/app/src/main/java/com/example/movies/ApiService.java
@@ -2,9 +2,10 @@ package com.example.movies;
 
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ApiService {
 
-    @GET("movie?field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&page=2&limit=40&token=SZ023GX-0Q9414N-N0J1M9R-DRJZB1F")
-    Single<MovieResponse> loadMovies();
+    @GET("movie?field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=40&token=SZ023GX-0Q9414N-N0J1M9R-DRJZB1F")
+    Single<MovieResponse> loadMovies(@Query("page") int page);
 }
--- Movies/app/src/main/java/com/example/movies/MainActivity.java
+++ Movies/app/src/main/java/com/example/movies/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.movies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.util.Log;
+
+import java.util.List;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Scheduler;
@@ -12,23 +16,20 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    private MainViewModel viewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ApiFactory.apiservice.loadMovies()
-                .subscribeOn(Schedulers.io())
-                .subscribeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<MovieResponse>() {
-                    @Override
-                    public void accept(MovieResponse movieResponse) throws Throwable {
-                        Log.d("MainActivity", movieResponse.toString());
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Throwable {
-                        Log.d("MainActivity", throwable.toString());
-                    }
-                });
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        viewModel.getMovies().observe(this, new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(List<Movie> movies) {
+                Log.d(TAG, movies.toString());
+            }
+        });
+        viewModel.loadMovies();
     }
 }
