73dd8d31b021c39f978ae4152d0bed35b8fdcca4
merox-vlados <merox-vlados@mail.ru>
2023-07-29 12:50:50 +0300

    commit 29.07.2023

--- Movies/app/src/main/java/com/example/movies/MainActivity.java
+++ Movies/app/src/main/java/com/example/movies/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.movies;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -15,21 +17,35 @@ import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActivity";
     private MainViewModel viewModel;
+
+    private RecyclerView recyclerViewMovies;
+    private MoviesAdapter moviesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+
+
         viewModel = new ViewModelProvider(this).get(MainViewModel.class);
         viewModel.getMovies().observe(this, new Observer<List<Movie>>() {
             @Override
             public void onChanged(List<Movie> movies) {
-                Log.d(TAG, movies.toString());
+                moviesAdapter.setMovies(movies);
             }
         });
         viewModel.loadMovies();
     }
+
+    private void initView () {
+        recyclerViewMovies = findViewById(R.id.recyclerViewMovies);
+        moviesAdapter = new MoviesAdapter();
+        recyclerViewMovies.setAdapter(moviesAdapter);
+        recyclerViewMovies.setLayoutManager(new GridLayoutManager(this, 2));
+    }
+
 }
