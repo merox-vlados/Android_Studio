da81cd98b1af58ee6cf1d8318996db335a233281
merox-vlados <merox-vlados@mail.ru>
2023-07-29 16:43:51 +0300

    commit 29.07.2023

--- Movies/app/src/main/java/com/example/movies/MainActivity.java
+++ Movies/app/src/main/java/com/example/movies/MainActivity.java
@@ -27,9 +27,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initView();
-
-
+        recyclerViewMovies = findViewById(R.id.recyclerViewMovies);
+        moviesAdapter = new MoviesAdapter();
+        recyclerViewMovies.setAdapter(moviesAdapter);
+        recyclerViewMovies.setLayoutManager(new GridLayoutManager(this, 2));
 
         viewModel = new ViewModelProvider(this).get(MainViewModel.class);
         viewModel.getMovies().observe(this, new Observer<List<Movie>>() {
@@ -39,13 +40,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         viewModel.loadMovies();
-    }
-
-    private void initView () {
-        recyclerViewMovies = findViewById(R.id.recyclerViewMovies);
-        moviesAdapter = new MoviesAdapter();
-        recyclerViewMovies.setAdapter(moviesAdapter);
-        recyclerViewMovies.setLayoutManager(new GridLayoutManager(this, 2));
     }
 
 }
