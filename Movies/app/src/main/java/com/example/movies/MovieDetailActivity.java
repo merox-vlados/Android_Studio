6abfdd18cf9062960aaffacb7ebf64186543142b
merox-vlados <merox-vlados@mail.ru>
2023-08-12 20:57:54 +0300

    commit 12.08.2023

--- Movies/app/src/main/java/com/example/movies/MovieDetailActivity.java
+++ Movies/app/src/main/java/com/example/movies/MovieDetailActivity.java
@@ -1,14 +1,17 @@
 package com.example.movies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -30,13 +33,11 @@ public class MovieDetailActivity extends AppCompatActivity {
     private TextView textViewTitle;
     private TextView textViewYear;
     private TextView textViewDescription;
-
     private RecyclerView recyclerViewTrailers;
     private TrailersAdapter trailersAdapter;
-
     private RecyclerView recyclerViewReview;
-
     private ReviewAdapter reviewAdapter;
+    private ImageView imageViewStar;
 
 
     @Override
@@ -85,10 +86,32 @@ public class MovieDetailActivity extends AppCompatActivity {
             }
         });
         viewModel.loadReviews(movie.getId());
-        MovieDao movieDao = MovieDatabase.getInstance(getApplication()).movieDao();
-        movieDao.insertMovie(movie)
-                .subscribeOn(Schedulers.io())
-                .subscribe();
+
+        Drawable starOff = ContextCompat.getDrawable(MovieDetailActivity.this, android.R.drawable.star_big_off);
+        Drawable starOn = ContextCompat.getDrawable(MovieDetailActivity.this, android.R.drawable.star_big_on);
+        viewModel.getFavouriteMovie(movie.getId())
+                .observe(this, new Observer<Movie>() {
+                    @Override
+                    public void onChanged(Movie movieFromDb) {
+                        if(movieFromDb == null) {
+                            imageViewStar.setImageDrawable(starOff);
+                            imageViewStar.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    viewModel.insertMovie(movie);
+                                }
+                            });
+                        } else {
+                            imageViewStar.setImageDrawable(starOn);
+                            imageViewStar.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    viewModel.removeMovie(movie.getId());
+                                }
+                            });
+                        }
+                    }
+                });
     }
 
     private void initViews() {
@@ -98,6 +121,7 @@ public class MovieDetailActivity extends AppCompatActivity {
         textViewDescription = findViewById(R.id.textViewDescription);
         recyclerViewTrailers = findViewById(R.id.recyclerViewTrailers);
         recyclerViewReview = findViewById(R.id.recyclerViewReviews);
+        imageViewStar = findViewById(R.id.imageViewStar);
     }
 
     public static Intent newIntent(Context context, Movie movie) {
--- Movies/app/src/main/java/com/example/movies/MovieDetailViewModel.java
+++ Movies/app/src/main/java/com/example/movies/MovieDetailViewModel.java
@@ -18,17 +18,19 @@ import io.reactivex.rxjava3.functions.Function;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MovieDetailViewModel extends AndroidViewModel {
-
     private static final String TAG = "MovieDetailViewModel";
-
     private final CompositeDisposable compositeDisposable = new CompositeDisposable();
-
     private final MutableLiveData<List<Trailer>> trailers = new MutableLiveData<>();
-
     private final MutableLiveData<List<Review>> reviews = new MutableLiveData<>();
+    private final MovieDao movieDao;
 
     public MovieDetailViewModel(@NonNull Application application) {
         super(application);
+        movieDao = MovieDatabase.getInstance(application).movieDao();
+    }
+
+    public LiveData<Movie> getFavouriteMovie(int movieId){
+        return movieDao.getFavouriteMovie(movieId);
     }
 
     public LiveData<List<Trailer>> getTrailers() {
@@ -85,6 +87,20 @@ public class MovieDetailViewModel extends AndroidViewModel {
                         Log.d(TAG, throwable.toString());
                     }
                 });
+        compositeDisposable.add(disposable);
+    }
+
+     public void insertMovie(Movie movie) {
+        Disposable disposable = movieDao.insertMovie(movie)
+                .subscribeOn(Schedulers.io())
+                .subscribe();
+        compositeDisposable.add(disposable);
+     }
+
+    public void removeMovie(int movieId) {
+        Disposable disposable = movieDao.insertMovie(movieId)
+                .subscribeOn(Schedulers.io())
+                .subscribe();
         compositeDisposable.add(disposable);
     }
 
