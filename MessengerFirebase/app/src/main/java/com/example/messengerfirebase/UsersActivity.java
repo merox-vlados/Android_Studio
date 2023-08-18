7761f140779b6b4a35fe427a51cb000ab270d4ce
merox-vlados <merox-vlados@mail.ru>
2023-08-18 15:25:07 +0300

    commit 18.08.2023

--- MessengerFirebase/app/src/main/java/com/example/messengerfirebase/UsersActivity.java
+++ MessengerFirebase/app/src/main/java/com/example/messengerfirebase/UsersActivity.java
@@ -9,10 +9,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,10 +31,34 @@ public class UsersActivity extends AppCompatActivity {
 
     private UsersViewModel viewModel;
 
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference("Users");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
+//        for(int i = 0; i < 10; i++) {
+//            User user = new User(
+//                    "id" + i, "name" + i, "lastName" + i, i, false
+//            );
+//            databaseReference.push().setValue(user);
+//        }
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    User value = dataSnapshot.getValue(User.class);
+                    Log.d("UsersActivity", value.toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         initViews();
         viewModel = new ViewModelProvider(this).get(UsersViewModel.class);
         observeViewModel();
